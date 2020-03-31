7752c4f4807c837a2efd77ad29b302f303ccbd7b
Francisco Aguilera <falven2000@hotmail.com>
2020-03-31 13:00:00 -0400

    Added akvclient and test.

---
+++ Azure/KeyVault/AKVClient.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * PHP version 7.4
+ *
+ * @category  Microsoft Azure
+ *
+ * @author    Fran Aguilera <fraguile@microsoft.com>
+ * @copyright 2020 Microsoft Corporation
+ */
+
+namespace Azure\KeyVault;
+
+class AKVClient
+{
+    // property declaration
+    public $var = 'Hello from within AKVClient.';
+
+    // method declaration
+    public function displayVar() {
+        echo $this->var;
+    }
+
+    // Method: POST, PUT, GET etc
+    // Data: array("param" => "value") ==> index.php?param=value
+    public function CallAPI($method, $url, $data = false)
+    {
+        $curl = curl_init();
+
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // Optional Authentication:
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
+include 'Azure/KeyVault/AKVClient.php';
+
 echo "Hello Azure!";
+
+$akvVar = new AKVClient();
+
+$akvVar->displayVar();
