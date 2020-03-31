6031c7101938ab44ec2dfd29afde1486d1601c6a
Francisco Aguilera <falven2000@hotmail.com>
2020-03-31 13:04:10 -0400

    Added newlines.

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include 'PHPAzure/KeyVault/AKVClient.php';
 
 use AKVClient;
 
-echo "Hello Azure!";
-echo "Initializing AKVClient";
+echo "Hello Azure!<br />";
+echo "Initializing AKVClient<br />";
 $akvVar = new AKVClient();
 
 $akvVar->displayVar();
