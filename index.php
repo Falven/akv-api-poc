463766bfa69a5eb4fdb270c70ac320b879a7ac5b
Francisco Aguilera <falven2000@hotmail.com>
2020-03-31 14:51:20 -0400

    Remove stuff.

--- index.php
+++ index.php
@@ -18,26 +18,6 @@ echo "Test Var: $testVar";
 $ac = new AKVClient();
 echo $ac->getMessage();
 
-include ("file1.php");
-
-class ClassB
-{
-
-    function __construct()
-    {
-    }
-
-    function callA()
-    {
-        $classA = new ClassA();
-        $name = $classA->getName();
-        echo $name;    //Prints John
-    }
-}
-
-$classb = new ClassB();
-$classb->callA();
-
 ?>
   </body>
 </html>
